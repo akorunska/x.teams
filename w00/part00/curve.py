42ff9f2bae62021d0cd8c8072e9b8596286d097e
akorunska <akorunska@gmail.com>
2019-01-21 13:18:42 +0200

    changed curve.py to receive name, surname and output file

--- w00/part00/curve.py
+++ w00/part00/curve.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def curve():
-    a = 4
-    b = 9
+def curve(name, surname, output_file="curve.png"):
+    a = len(name)
+    b = len(surname)
 
     y, x = np.ogrid[-20:20:100j, -10:30:100j]
 
     plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
     plt.grid()
-    plt.show()
+    plt.savefig(output_file)
 
 
 if __name__ == '__main__':
-    curve()
+    curve("Анна", "Корунская")
