5c7c3b6e4084b6eefd62c50506ee6605f96cd8de
akorunska <akorunska@gmail.com>
2019-01-21 10:42:26 +0200

    deleted printing

--- w00/part00/curve.py
+++ w00/part00/curve.py
@@ -7,7 +7,7 @@ def curve():
     b = 9
 
     y, x = np.ogrid[-20:20:100j, -10:30:100j]
-    print(x)
+
     plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
     plt.grid()
     plt.show()
