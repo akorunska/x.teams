6563feb227c2c14d27ac6839a9ea3e4326321961
akorunska <akorunska@gmail.com>
2019-01-21 10:41:43 +0200

    curve.py displays eliptic curve: y^2 = x^3 + 4*x^2 + 9

--- w00/part00/curve.py
+++ w00/part00/curve.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
+def curve():
     a = 4
     b = 9
 
-    y, x = np.ogrid[-20:20:100j, -20:20:100j]
+    y, x = np.ogrid[-20:20:100j, -10:30:100j]
+    print(x)
     plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
     plt.grid()
     plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    curve()
