762f8da524a28634a1b4f7278b856ae776503848
akorunska <akorunska@gmail.com>
2019-01-22 15:09:25 +0200

    basic keys and addresses functionality in wallet.py generating private keys converting hex private keys to wif receiving public keys and addresses from private key

---
+++ w00/part01/wallet.py
@@ -0,0 +1,82 @@
+import binascii
+import codecs
+import secrets
+import hashlib
+import base58
+import ecdsa
+
+
+def generate_private_key():
+    order_of_elliptic_curve = 1.158 * 10**77
+    privkey = hex(int(order_of_elliptic_curve))
+
+    while float(int(privkey, 16)) >= order_of_elliptic_curve:
+        randbits = secrets.randbits(1024)
+        privkey = hashlib.sha256(str(randbits).encode('utf-8')).hexdigest()
+    return privkey
+
+
+def read_file_contents(filename):
+    f = open(filename)
+    return f.read().strip().upper()
+
+
+def convert_private_key_to_wif(filename='privkey'):
+    hex_privkey = read_file_contents(filename)
+    hex_privkey_extended = "80" + hex_privkey
+
+    hashed1 = hashlib.sha256(binascii.unhexlify(hex_privkey_extended)).hexdigest()
+    hashed2 = hashlib.sha256(binascii.unhexlify(hashed1)).hexdigest()
+    final_key = hex_privkey_extended + hashed2[:8]
+
+    return base58.b58encode(binascii.unhexlify(final_key)).decode('ascii')
+
+
+def get_public_key_from_private_key(privkey):
+    pubkey = {"uncompressed": 0, "compressed": 0}
+    privkey_bytes = codecs.decode(privkey, 'hex')
+    # Get ECDSA public key
+    key = ecdsa.SigningKey.from_string(privkey_bytes, curve=ecdsa.SECP256k1).verifying_key
+    key_bytes = key.to_string()
+    key_hex = codecs.encode(key_bytes, 'hex')
+    pubkey['uncompressed'] = b"04" + key_hex
+
+    x = key_hex[0:(int(len(key_hex)/2))]
+    y = key_hex[(int(len(key_hex)/2)):]
+    if int(y, 16) % 2 == 0:
+        pubkey["compressed"] = b'02' + x
+    else:
+        pubkey["compressed"] = b'03' + x
+    return pubkey
+
+
+def get_address_from_private_key(privkey, use_compressed=True):
+    pubkeys = get_public_key_from_private_key(privkey)
+
+    # using compressed key as default to generate and address as it becoming default option for many bitcoin clients
+    # uncompressed key can be used as well by use_compressed=False
+    if use_compressed:
+        pubkey = pubkeys["compressed"]
+    else:
+        pubkey = pubkeys["uncompressed"]
+
+    pubkey_sha_encrypted = hashlib.sha256(binascii.unhexlify(pubkey)).hexdigest()
+
+    ripemd160 = hashlib.new('ripemd160')
+    ripemd160.update(binascii.unhexlify(pubkey_sha_encrypted))
+    pubkey_ripemd_encrypted = ripemd160.hexdigest()
+
+    mainnet_network_bytes = "00"
+    key_with_network_bytes = mainnet_network_bytes + pubkey_ripemd_encrypted
+
+    key_hashed1 = hashlib.sha256(binascii.unhexlify(key_with_network_bytes)).hexdigest()
+    key_hashed2 = hashlib.sha256(binascii.unhexlify(key_hashed1)).hexdigest()
+    address = key_with_network_bytes + key_hashed2[:8]
+    return base58.b58encode(binascii.unhexlify(address)).decode('ascii')
+
+
+if __name__ == '__main__':
+    private = read_file_contents("privkey")
+    # print(private)
+    print(get_address_from_private_key(private, use_compressed=False))
+
