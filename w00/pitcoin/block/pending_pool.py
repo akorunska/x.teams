940bf8ea2373442c1962ee90a65667461fd48804
Anna Korunska <akorunska@gmail.com>
2019-01-24 23:43:32 +0200

    updating pending pool and its tests

--- w00/pitcoin/block/pending_pool.py
+++ w00/pitcoin/block/pending_pool.py
@@ -3,8 +3,6 @@ from pitcoin.transaction.tx_validator import check_tx_validity
 import pickle
 from pathlib import Path
 
-# storage_filepath = '../storage/mempool.txt'
-
 
 class Storage:
     def __init__(self, filepath='../storage/mempool.txt'):
@@ -12,47 +10,14 @@ class Storage:
 
     def get_validated_deserialized_transaction(self, data):
         tx = Deserializer.deserialize_transaction(data)
-        try:
-            if check_tx_validity(tx):
-                return tx
-            else:
-                return None
-        except Exception as e:
+        if check_tx_validity(tx):
+            return tx
+        else:
             return None
 
     def add_serialized_transaction_to_mempool(self, data):
         tx = self.get_validated_deserialized_transaction(data)
         self.add_transaction_to_mempool(tx)
-
-    def add_transaction_to_mempool(self, tx):
-        tx_list = []
-        if not Path(self.storage_filepath).is_file():
-            Path(self.storage_filepath).touch()
-        else:
-            with open(self.storage_filepath, 'rb') as fp:
-                tx_list = pickle.load(fp)
-
-        tx_list.append(tx)
-        with open(self.storage_filepath, 'wb+') as fp:
-            pickle.dump(tx_list, fp)
-
-    def delete_last_transaction_from_mempool(self):
-        tx_list = []
-        if not Path(self.storage_filepath).is_file():
-            Path(self.storage_filepath).touch()
-        else:
-            with open(self.storage_filepath, 'rb') as fp:
-                tx_list = pickle.load(fp)
-
-        tx_list.pop()
-        with open(self.storage_filepath, 'wb+') as fp:
-            pickle.dump(tx_list, fp)
-
-    def delete_all_transactions_from_mempool(self):
-        tx_list = []
-
-        with open(self.storage_filepath, 'wb+') as fp:
-            pickle.dump(tx_list, fp)
 
     def get_all_transactions(self):
         tx_list = []
@@ -61,8 +26,24 @@ class Storage:
         else:
             with open(self.storage_filepath, 'rb') as fp:
                 tx_list = pickle.load(fp)
-
         return tx_list
+
+    def add_transaction_to_mempool(self, tx):
+        tx_list = self.get_all_transactions()
+        tx_list.append(tx)
+        with open(self.storage_filepath, 'wb+') as fp:
+            pickle.dump(tx_list, fp)
+
+    def delete_last_transaction_from_mempool(self):
+        tx_list = self.get_all_transactions()
+        tx_list.pop()
+        with open(self.storage_filepath, 'wb+') as fp:
+            pickle.dump(tx_list, fp)
+
+    def delete_all_transactions_from_mempool(self):
+        tx_list = []
+        with open(self.storage_filepath, 'wb+') as fp:
+            pickle.dump(tx_list, fp)
 
     def get_three_last_transactions(self):
         tx_list = self.get_all_transactions()
@@ -73,25 +54,5 @@ class Storage:
             return tx_list
 
     def get_transactions_count(self):
-        tx_list = []
-        if not Path(self.storage_filepath).is_file():
-            Path(self.storage_filepath).touch()
-        else:
-            with open(self.storage_filepath, 'rb') as fp:
-                tx_list = pickle.load(fp)
-
+        tx_list = self.get_all_transactions()
         return len(tx_list)
-
-
-# 00221CAEuYPgEYnyepAF51AiKdCvPfTgjoMieU1GMRXMjXGVxC43GtonDHJ5YY8jQp2RKHEA50e829ca678c60031a11b990fea865e03ba35d0579aa62750b918b98c4b935d803ecc57a4bb2fc2ab1193a87fca5386d71516aca89df267fc907bcb3b84d396a0580fae8ca3bbf6b7a016d5f247feb89efbd0412e84524d0746520bd45ea45112e4ce76474636eb36f67b5407249742ee54b97322184135fe0251511f5149328
-
-if __name__ == '__main__':
-    tx = Storage.get_validated_deserialized_transaction(b'00091GFfoqR4Z4BZEy75Nd9CRMTKAev3oukY2Q1GMRXMjXGVxC43GtonDHJ5YY8jQp2RKHEA50e829ca678c60031a11b990fea865e03ba35d0579aa62750b918b98c4b935d803ecc57a4bb2fc2ab1193a87fca5386d71516aca89df267fc907bcb3b84d396a7388be31865e41be006c14ef7033be6301105563f1bf16cc257fc7af43630bc735b075e1be42c9a37799ed69a1e49de6a14d3390550718e78db3dbd454a08021')
-    # Storage.add_transaction_to_mempool(tx)
-    # Storage.delete_last_transaction_from_mempool()
-
-    Storage.delete_all_transactions_from_mempool()
-
-    txs = Storage.get_three_last_transactions()
-    for t in txs:
-        print(t)
--- w00/pitcoin/tests/test_pending_pool.py
+++ w00/pitcoin/tests/test_pending_pool.py
@@ -1,4 +1,5 @@
 import unittest
+from pitcoin import PROJECT_ROOT
 from pitcoin.block import Storage
 from pitcoin.transaction import Transaction
 from pitcoin.transaction import Serializer
@@ -11,8 +12,10 @@ class TestPendingPool(unittest.TestCase):
     #         10
     #     ).sign_transaction("936abdc0429eb4b38a045fcb8f531ff7cf3888c3a83797df5d033106c4ea6a20")
 
+    storage_filepath = PROJECT_ROOT + "/storage/mempool_test.txt"
+
     def test_adding_to_mempool(self):
-        storage = Storage("../storage/mempool_test.txt")
+        storage = Storage(self.storage_filepath)
         storage.delete_all_transactions_from_mempool()
         tx = Transaction("sndr", "rspt", 20)
         storage.add_transaction_to_mempool(tx)
@@ -21,7 +24,7 @@ class TestPendingPool(unittest.TestCase):
         self.assertTrue(tx in tx_list)
 
     def test_adding_correct_serialized_tx_to_mempool(self):
-        storage = Storage("../storage/mempool_test.txt")
+        storage = Storage(self.storage_filepath)
         storage.delete_all_transactions_from_mempool()
         tx = Transaction(
             "1GFfoqR4Z4BZEy75Nd9CRMTKAev3oukY2Q",
@@ -35,7 +38,7 @@ class TestPendingPool(unittest.TestCase):
         self.assertTrue(tx in tx_list)
 
     def test_adding_incorrect_serialized_tx_to_mempool(self):
-        storage = Storage("../storage/mempool_test.txt")
+        storage = Storage(self.storage_filepath)
         storage.delete_all_transactions_from_mempool()
         tx = Transaction("sndr", "rspt", 20)
         storage.add_serialized_transaction_to_mempool(Serializer.serialize_transaction(tx))
@@ -44,7 +47,7 @@ class TestPendingPool(unittest.TestCase):
         self.assertFalse(tx in tx_list)
 
     def test_deleting_last_transaction(self):
-        storage = Storage("../storage/mempool_test.txt")
+        storage = Storage(self.storage_filepath)
         storage.delete_all_transactions_from_mempool()
         tx1 = Transaction("sndr", "rspt", 20)
         tx2 = Transaction("sndr1", "rspt2", 20)
@@ -62,7 +65,7 @@ class TestPendingPool(unittest.TestCase):
         self.assertFalse(tx3 in tx_list)
 
     def test_deleting_all_transactions(self):
-        storage = Storage("../storage/mempool_test.txt")
+        storage = Storage(self.storage_filepath)
         storage.delete_all_transactions_from_mempool()
         tx1 = Transaction("sndr", "rspt", 20)
         tx2 = Transaction("sndr1", "rspt2", 20)
@@ -80,4 +83,43 @@ class TestPendingPool(unittest.TestCase):
         self.assertFalse(tx1 in tx_list)
         self.assertFalse(tx2 in tx_list)
         self.assertFalse(tx3 in tx_list)
+
+    def test_get_three_last_tx_empty_list(self):
+        storage = Storage(self.storage_filepath)
+        storage.delete_all_transactions_from_mempool()
+
+        storage.get_three_last_transactions()
+        self.assertEqual(0, storage.get_transactions_count())
+
+    def test_get_three_last_tx_two_items_list(self):
+        storage = Storage(self.storage_filepath)
+        storage.delete_all_transactions_from_mempool()
+        tx1 = Transaction("sndr1", "rspt1", 20)
+        tx2 = Transaction("sndr2", "rspt2", 20)
+        storage.add_transaction_to_mempool(tx1)
+        storage.add_transaction_to_mempool(tx2)
+
+        last_tx_list = storage.get_three_last_transactions()
+        self.assertEqual(2, len(last_tx_list))
+        self.assertTrue(tx1 in last_tx_list)
+        self.assertTrue(tx2 in last_tx_list)
+
+    def test_get_three_last_tx_many_items_list(self):
+        storage = Storage(self.storage_filepath)
+        storage.delete_all_transactions_from_mempool()
+        tx1 = Transaction("sndr1", "rspt1", 20)
+        tx2 = Transaction("sndr2", "rspt2", 20)
+        tx3 = Transaction("sndr3", "rspt4", 20)
+        tx4 = Transaction("sndr3", "rspt4", 20)
+        storage.add_transaction_to_mempool(tx1)
+        storage.add_transaction_to_mempool(tx2)
+        storage.add_transaction_to_mempool(tx3)
+        storage.add_transaction_to_mempool(tx4)
+
+        last_tx_list = storage.get_three_last_transactions()
+        self.assertEqual(3, len(last_tx_list))
+        self.assertFalse(tx1 in last_tx_list)
+        self.assertTrue(tx2 in last_tx_list)
+        self.assertTrue(tx3 in last_tx_list)
+        self.assertTrue(tx4 in last_tx_list)
 
