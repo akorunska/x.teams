cf948d5e7857bcc23b2a9f976b5bd81ff655f0b2
Anna Korunska <akorunska@gmail.com>
2019-01-26 01:00:23 +0200

    pushing block to the api, initializer script creating genesis block

---
+++ w00/pitcoin/network/chain.py
@@ -0,0 +1,37 @@
+from pitcoin.transaction import Deserializer, Transaction
+from pitcoin.transaction.tx_validator import check_tx_validity
+import pickle
+from pathlib import Path
+from pitcoin import PROJECT_ROOT
+
+
+class BlocksStorage:
+    def __init__(self, filepath=PROJECT_ROOT+'/storage/blocks.txt'):
+        self.storage_filepath = filepath
+
+    def get_all_blocks(self):
+        blocks_list = []
+        if not Path(self.storage_filepath).is_file():
+            Path(self.storage_filepath).touch()
+        else:
+            with open(self.storage_filepath, 'rb') as fp:
+                try:
+                    blocks_list = tx_list = pickle.load(fp)
+                except EOFError:
+                    blocks_list = []
+        return blocks_list
+
+    def add_block_to_storage(self, b):
+        blocks_list = self.get_all_blocks()
+        blocks_list.append(b)
+        with open(self.storage_filepath, 'wb+') as fp:
+            pickle.dump(blocks_list, fp)
+
+    def delete_all_blocks_from_mempool(self):
+        blocks_list = []
+        with open(self.storage_filepath, 'wb+') as fp:
+            pickle.dump(blocks_list, fp)
+
+    def get_blocks_count(self):
+        blocks_list = self.get_all_blocks()
+        return len(blocks_list)
