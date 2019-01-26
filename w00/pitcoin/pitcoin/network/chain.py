e883333d14e56f7171902c02ad4054576c88d260
Anna Korunska <akorunska@gmail.com>
2019-01-26 18:52:48 +0200

    updated adding new block for genesis block case

--- w00/pitcoin/pitcoin/network/chain.py
+++ w00/pitcoin/pitcoin/network/chain.py
@@ -25,6 +25,7 @@ class BlocksStorage:
     def add_block_to_storage(self, b: Block):
         blocks_list = self.get_all_blocks()
         # todo check block`s validity
+        print(blocks_list)
         if len(blocks_list) != 0:
             if blocks_list[-1].hash_value != b.previous_hash:
                 return False
