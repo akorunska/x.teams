4dbec17e708da68bdf621970cb8a566ce82dc988
Anna Korunska <akorunska@gmail.com>
2019-02-08 12:08:00 +0200

    some preparations for new miner_cli

--- module-3/initializer.py
+++ module-3/initializer.py
@@ -12,9 +12,17 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
+
+def clear_storage(path):
     try:
         os.remove(path + ".blocks.txt")
+    except OSError:
+        pass
+    try:
         os.remove(path + ".mempool.txt")
+    except OSError:
+        pass
+    try:
         os.remove(path + ".utxopool.txt")
     except OSError:
         pass
@@ -28,5 +36,14 @@ def genesis_block_setup():
     requests.post(api_url + '/chain/block', str(genesis_block))
 
 
+def get_known_nodes():
+    pass
+
+
+
 make_sure_path_exists('pitcoin_modules/storage/')
+clear_storage('pitcoin_modules/storage/')
+
+# get all known nodes from the config file
+# if there are no nodes, create own genesis block
 genesis_block_setup()
