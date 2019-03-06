0f3fb210f3cadfc0d712dd695ac317581b12e9ba
Anna Korunska <akorunska@gmail.com>
2019-03-06 22:26:19 +0200

    outputting own address balance in the browser with web3

---
+++ module-7/wallet/src/components/Root.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{balance}}</h2>
+
+  </div>
+
+</template>
+
+<script>
+  import Web3 from 'web3';
+  import settings from '../settings';
+  import axios from 'axios';
+
+export default {
+  name: 'Root',
+  async created() {
+    this.setupWeb3();
+    await this.getBalance();
+  },
+  data () {
+    return {
+      web3js: '',
+      balance: 0,
+      balanceEth: 0,
+    }
+  },
+  methods: {
+    async setupWeb3() {
+      this.web3js = new Web3(web3.currentProvider);
+    },
+    async getBalance() {
+      let response = await this.web3js.eth.getBalance('0xa3E1aB154F40d984E56fF07d1EEa46a50BD8ee36');
+      this.balance = this.web3js.utils.fromWei(response);
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ module-7/wallet/src/settings.js
@@ -0,0 +1,3 @@
+module.exports = {
+  // infura_endpoint: "https://ropsten.infura.io/v3/f41b24d25f074ab4978ebab0d26dbf9c",
+};
