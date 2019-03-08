7fd068113b221feaf9075e29a9866a670fe65b56
Anna Korunska <akorunska@gmail.com>
2019-03-08 15:54:47 +0200

    increment/decrement example with vuex

---
+++ module-7/wallet/src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="container">
+      {{msg}}
+
+      <div id="app">
+        <p>{{ count }}</p>
+        <p>
+          <button @click="increment">+</button>
+          <button @click="decrement">-</button>
+        </p>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+
+  export default {
+    name: "Home",
+    data() {
+      return {
+        msg: "home",
+      }
+    },
+    computed: {
+      ...mapState({
+        count: state => state.count
+      })
+    },
+    methods: {
+      increment () {
+        this.$store.commit('increment');
+      },
+      decrement () {
+        this.$store.commit('decrement');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
--- module-7/wallet/src/components/Root.vue
+++ module-7/wallet/src/components/Root.vue
@@ -1,40 +1,56 @@
 <template>
   <div>
-    <h2>{{balance}}</h2>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="navbar-brand"> Best Ethereum Wallet </div>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+          <div v-for="tab in tabs">
+            <li v-if="tab === active_component" class="nav-item active">
+              <a class="nav-link" href="#" v-on:click="updateContent(tab)"> {{ tab }} <span class="sr-only">(current)</span></a>
+            </li>
+            <li v-else class="nav-item">
+              <a class="nav-link" href="#" v-on:click="updateContent(tab)"> {{ tab }} </a>
+            </li>
+          </div>
+
+        </ul>
+      </div>
+    </nav>
+
+    <component v-bind:is="content"></component>
 
   </div>
-
 </template>
 
 <script>
-  import Web3 from 'web3';
-  import settings from '../settings';
-  import axios from 'axios';
+  import Home from './Home'
+  import Balances from './Balances'
 
-export default {
-  name: 'Root',
-  async created() {
-    this.setupWeb3();
-    await this.getBalance();
-  },
-  data () {
-    return {
-      web3js: '',
-      balance: 0,
-      balanceEth: 0,
-    }
-  },
-  methods: {
-    async setupWeb3() {
-      this.web3js = new Web3(web3.currentProvider);
+  export default {
+    name: "Navbar",
+    created() {
+      this.updateContent(this.active_component)
     },
-    async getBalance() {
-      let response = await this.web3js.eth.getBalance('0xa3E1aB154F40d984E56fF07d1EEa46a50BD8ee36');
-      this.balance = this.web3js.utils.fromWei(response);
-    }
-  },
-}
-
+    data() {
+      return {
+        tabs: ['Home', '', '', '', '', 'Balances'],
+        active_component: 'Home',
+        content: "",
+      };
+    },
+    methods: {
+      updateContent(data) {
+        this.active_component = data;
+        if (data === 'Home') {
+          this.content = Home;
+        } else if (data === 'Balances') {
+          this.content = Balances;
+        }
+      },
+    },
+  }
 </script>
 
 <style scoped>
