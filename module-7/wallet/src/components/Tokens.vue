40a53d358d3371e2ad6882d5c90e553ac5d1f9a2
Anna Korunska <akorunska@gmail.com>
2019-03-10 23:02:36 +0200

    naught coin info when inputting its address

--- module-7/wallet/src/components/Tokens.vue
+++ module-7/wallet/src/components/Tokens.vue
@@ -3,7 +3,40 @@
     <br>
 
     <div v-if="$store.state.loggedIn">
-      Tokens
+
+      <div>
+        <form class="form-group">
+          <div>
+            <label for="addressInput"> Enter ERC20 token address: </label>
+            <input id="addressInput" type="text" class="form-control  my-1 mr-sm-2" v-model="contractAddress">
+          </div>
+
+          <button type="submit" class="btn btn-outline-info my-1 mr-sm-2" v-on:click="getInfoFromContract">Lets go</button>
+        </form>
+      </div>
+
+      <div v-if="contractInfo">
+        <div class="card my-1 mr-sm-2 border-info">
+          <div class="card-body">
+            <h5 class="card-title"> ERC20 Coin at {{ contractAddress }}</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <table class="table">
+                  <tbody>
+                  <tr  v-for="(item, key, index) in contractInfo">
+                    <td> {{ key }}</td>
+                    <td> {{ item }} </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+
+
     </div>
 
     <div v-else>
@@ -15,22 +48,38 @@
 </template>
 
 <script>
-    export default {
-      name: "Tokens.vue",
-      created() {
-        this.setupWeb3();
+  import Web3 from 'web3';
+  let abi = require('human-standard-token-abi');
+
+  export default {
+    name: "Tokens.vue",
+    created() {
+      this.setupWeb3();
+    },
+    data() {
+      return {
+        web3js: '',
+        contractAddress: '',
+        contractInfo: undefined,
+      };
+    },
+    methods: {
+      async setupWeb3() {
+        this.web3js = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws"));
       },
-      data() {
-        return {
-          web3js: '',
+      async getInfoFromContract() {
+        const contract = new this.web3js.eth.Contract(abi, this.contractAddress);
+
+        this.contractInfo = {
+          name: await contract.methods.name.call(),
+          symbol:  await contract.methods.symbol.call(),
+          totalSupply: await contract.methods.totalSupply.call(),
         };
-      },
-      methods: {
-        async setupWeb3() {
-          this.web3js = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws"));
-        },
+
+        console.log(this.contractInfo)
       }
     }
+  }
 </script>
 
 <style scoped>
